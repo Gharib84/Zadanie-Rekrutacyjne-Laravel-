4b494be7982f6cac1aafc7374493b6deb3f6b8bc
Gharib84e <ITprofessional63@gmail.com>
2025-03-13 22:31:31 +0100

    basic router configured

--- app/Http/Controllers/PetsController.php
+++ app/Http/Controllers/PetsController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class PetsController extends Controller
 {
-    //
+
+    public function index(): View
+    {
+        $response = Http::get('https://petstore.swagger.io/v2/pet/findByStatus', [
+            'findByStatus' => 'available'
+        ]);
+
+        $data = $response->json();
+
+        if ($response->successful()) {
+            return view('welcome', [
+                'pets' => $data
+            ]);
+        }
+
+        return view('welcome', [
+            'pets' => []
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PetsController::class, 'index']);
